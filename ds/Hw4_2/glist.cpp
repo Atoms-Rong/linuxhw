2bd4d160023986515ab15a085ed099d734582b8d
Ratoms <2547314692@qq.com>
2023-10-23 17:45:12 +0800

    equal2

--- ds/Hw4_2/glist.cpp
+++ ds/Hw4_2/glist.cpp
@@ -75,21 +75,6 @@ private:
         if(s->type != t->type) return false;
         if(s->type == 0){
             if(s->info.ref != t->info.ref) return false;
-        }
-        else if(s->type == 1){
-            if(s->info.val != t->info.val) return false;
-        }
-        else if(s->type == 2){
-            if(!Equal(s->info.hlink, t->info.hlink)) return false;
-        }
-        return Equal(s->tlink, t->tlink);
-    }
-    bool Equal(node *s, node* t){
-        if(s == NULL && t == NULL) return true;
-        if(s == NULL || t == NULL) return false;
-        if(s->type != t->type) return false;
-        if(s->type == 0){
-            if(s->info.ref != t->info.ref) return false;
             if(s->info.ref > 0){
                 --s->info.ref;
                 --t->info.ref;
